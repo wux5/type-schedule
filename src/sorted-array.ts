39ee0cf321420258e8e8d2dee3e3cf3466301b6e
John Wu <wuj999@gmail.com>
2017-09-10 03:18:59 -0700

    feat: Implement SortedArray
    Add test framework with mocha and istanbul (nyc) for code coverage

---
+++ src/sorted-array.ts
@@ -0,0 +1,76 @@
+export interface IComparer {
+  <T>(a: T, b: T): number;
+}
+
+function defaultCompare<T>(a: T, b: T) {
+  if (a === b) {
+    return 0;
+  }
+  return a < b ? -1 : 1;
+}
+
+export class SortedArray {
+  array: Array<any> = [];
+  compare: IComparer;
+
+  constructor(array: Array<any>, compare?: IComparer) {
+    this.compare = compare || defaultCompare;
+
+    const length: number = array.length;
+    let index: number = 0;
+    while (index < length) {
+      this.insert(array[index++]);
+    }
+  }
+
+  insert<T>(element: T): SortedArray {
+    const array: Array<T> = this.array;
+    const length: number = array.length;
+    const compare: IComparer = this.compare;
+    let index: number = length - 1;
+
+    while (index >= 0) {
+      if (compare(element, array[index]) >= 0) {
+        array.splice(index + 1, 0, element);
+        return this;
+      }
+      index--;
+    }
+    array.splice(0, 0, element);
+    return this;
+  }
+
+  search<T>(element: T): number {
+    const array: Array<T> = this.array;
+    const compare: IComparer = this.compare;
+    let low: number = 0;
+    let high: number = array.length;
+    if (high > 0) {
+      if (array[low] === element) {
+        return low;
+      } else if (array[high - 1] === element) {
+        return high - 1;
+      }
+    }
+    while (high > low) {
+      let index: number = (high + low) / 2 >>> 0;
+      let ordering: number = compare(element, array[index]);
+      if (ordering < 0) {
+        high = index;
+      } else if (ordering > 0) {
+        low = index + 1;
+      } else {
+        return index;
+      }
+    }
+    return -1;
+  }
+
+  remove<T>(element: T): SortedArray {
+    const index = this.search(element);
+    if (index > -1) {
+      this.array.splice(index, 1);
+    }
+    return this;
+  }
+}
---
+++ test/sorted-array.spec.ts
@@ -0,0 +1,64 @@
+import { IComparer, SortedArray } from "../src/sorted-array";
+import { expect } from "chai";
+
+describe('SortedArray class', () => {
+  describe('constructor, .insert', () => {
+    context('when compare function is not supplied', () => {
+      it('should create an instance with the default compare', () => {
+        const array = new SortedArray([2, 1, 3, 5, 4, 3]);
+        expect(array.array).to.eql([1, 2, 3, 3, 4, 5]);
+      });
+    });
+    context('when compare function is supplied', () => {
+      it('should create an instance using the supplied compare function', () => {
+        const compare = function <T>(a: T, b: T): number {
+          if (a === b) {
+            return 0;
+          }
+          return a < b ? 1 : -1;
+        }
+        const array = new SortedArray([2, 1, 3, 5, 4, 3], compare);
+        expect(array.array).to.eql([5, 4, 3, 3, 2, 1]);
+      });
+    });
+  });
+  describe('.search', () => {
+    context('when the element is found', () => {
+      it('should return the index', () => {
+        const array = new SortedArray([1, 2, 3, 4, 5]);
+        for (let i = 0; i < 5; i++) {
+          expect(array.search(i + 1)).to.eql(i);
+        }
+      });
+    });
+    context('when the element is not found', () => {
+      it('should return the -1', () => {
+        let array = new SortedArray([1, 2, 3, 4, 5]);
+        expect(array.search(0)).to.eql(-1);
+        expect(array.search(6)).to.eql(-1);
+        array = new SortedArray([]);
+        expect(array.search(1)).to.eql(-1);
+      });
+    });
+  });
+  describe('.remove', () => {
+    context('when the element is found', () => {
+      it('should remove the element', () => {
+        const array = new SortedArray([1, 2, 3, 4, 5]);
+        array.remove(1);
+        expect(array.array).to.eql([2, 3, 4, 5]);
+        array.remove(5);
+        expect(array.array).to.eql([2, 3, 4]);
+        array.remove(3);
+        expect(array.array).to.eql([2, 4]);
+      });
+    });
+    context('when the element is not found', () => {
+      it('should not change the array', () => {
+        const array = new SortedArray([1, 2, 3, 4, 5]);
+        array.remove(6);
+        expect(array.array).to.eql([1, 2, 3, 4, 5]);
+      });
+    });
+  });
+});
